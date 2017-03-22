507c0bcbea0038d5491ebc26c235495eca91d91f
Marcin Moch <Marcin.Moch@pkobp.pl>
2017-03-22 16:34:38 +0100

    [SPOX_0]Added get_next_number

--- SPOX_0/main.c
+++ SPOX_0/main.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
-#define gc getchar
+#define gc getchar_unlocked
+// #define gc getchar //bezpieczniejsze, ale wolniejsze
+
 //ASCII char codes:
 //32 = whitespace
 //10 = new line
+//48 = 0 (zero - number)
+//...
+//57 = 9 (nine - number)
+
+int current_number = 0;
+inline void get_next_number();
 
 int main() {
-    register char c; /* register -> przechowywanie zmiennej w rejestrze procesora
-                        znacznie szybsze niż RAM - brak adresu - nie można zrobić pointera */
-
-    while ((c = gc()) != EOF) {
-        printf("char code: %d", c);
-    }
-    
-    printf("EOF");
+    get_next_number();
+    printf("current_number: %d", current_number);
     
     return 0;
+}
+
+inline void get_next_number() {
+    /* register -> przechowywanie zmiennej w rejestrze procesora
+    znacznie szybsze niż RAM - brak adresu - nie można zrobić pointera */
+    register int c;
+    while ((c = gc()) != 'E') //TODO: for testing - change to EOF later
+    {
+        if (c >= '0' && c <= '9')
+        {
+            current_number = current_number * 10 + (c - '0'); //to samo co current_number * 10 + (c - '0')
+        }
+        else if (c == '\n')
+        {
+            //TODO: dummy just for testing locally -> remove later
+        }
+        else
+        { 
+            current_number = 0;
+        }
+    }
 }
