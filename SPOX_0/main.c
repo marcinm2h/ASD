de6ebe6839e2cae3ddde7f824f5c2f2e8e626bfe
Marcin Moch <Marcin Moch>
2017-03-22 21:36:31 +0100

    [SPOX_0]Digit counting

--- SPOX_0/main.c
+++ SPOX_0/main.c
@@ -2,27 +2,29 @@
 #define gc getchar_unlocked
 // #define gc getchar //bezpieczniejsze, ale wolniejsze
 
-//ASCII char codes:
-//32 = whitespace
-//10 = new line
-//48 = 0 (zero - number)
-//...
-//57 = 9 (nine - number)
+// ASCII char codes:
+// 32 = whitespace
+// 10 = new line
+// 48 = 0 (zero - number)
+// ...
+// 57 = 9 (nine - number)
 
 int current_number = 0;
 int sum = 0;
 int read_chars_count = 0;
 inline int get_next_number();
+inline int get_other_base_digits(int base);
 
 int main() {
 
-    while (!feof(stdin)) {
+    while (!feof(stdin))
+    {
         current_number = get_next_number();
         sum = sum + current_number;
         current_number = 0;
     }
     // printf("sum: %d, read_chars_count: %d\n", sum, read_chars_count);
-    printf("%d\n%d", sum, read_chars_count);
+    printf("sum: %d\nread_chars_counts: %d\ndigits: %d\n", sum, read_chars_count, get_other_base_digits(2));
     return 0;
 }
 
@@ -30,7 +32,8 @@ inline int get_next_number() {
     /* register -> przechowywanie zmiennej w rejestrze procesora
     znacznie szybsze niż RAM - brak adresu - nie można zrobić pointera  - nie można użyć w funkcjach */
     register int input_char = gc();
-    while (input_char != EOF && input_char != ' ') {
+    while (input_char != EOF && input_char != ' ')
+    {
         if (input_char >= '0' && input_char <= '9')
         {
             current_number = (current_number << 1) + (current_number << 3) + (input_char - '0'); // == current_number * 10 + (c - '0')
@@ -39,4 +42,27 @@ inline int get_next_number() {
         input_char = gc();
     }
     return current_number;
+}
+
+inline int get_other_base_digits(int base) {
+    register int digit = 1;
+    register int number = sum;
+
+    if (base == 0)
+    {
+        return 0;
+    }
+    else if (base == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        while (number > base)
+        {
+            digit += 1;
+            number = number / base;
+        }
+        return digit;
+    }
 }
