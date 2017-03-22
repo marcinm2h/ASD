6642061dc5bb0d84e2fcd94ad154d0d903cf1727
Marcin Moch <Marcin.Moch@pkobp.pl>
2017-03-22 17:07:28 +0100

    [SPOX_0]Improved logic

--- SPOX_0/main.c
+++ SPOX_0/main.c
@@ -10,33 +10,33 @@
 //57 = 9 (nine - number)
 
 int current_number = 0;
-inline void get_next_number(int input_char);
+int sum = 0;
+int read_chars_count = 0;
+inline int get_next_number();
 
 int main() {
 
-    /* register -> przechowywanie zmiennej w rejestrze procesora
-    znacznie szybsze niż RAM - brak adresu - nie można zrobić pointera */
-    register int input_char;
-    while ((input_char = gc()) != EOF) {
-        get_next_number(input_char);
-        // printf("current_number: %d", current_number);
+    while (!feof(stdin)) {
+        current_number = get_next_number();
+        sum = sum + current_number;
+        current_number = 0;
     }
-    
+    // printf("sum: %d, read_chars_count: %d\n", sum, read_chars_count);
+    printf("%d\n%d", sum, read_chars_count);
     return 0;
 }
 
-inline void get_next_number(int input_char) {
-    if (input_char >= '0' && input_char <= '9')
-    {
-        current_number = (current_number << 1) + (current_number << 3) + (input_char - '0'); // == current_number * 10 + (c - '0')
-        printf("current_number: %d", current_number);
+inline int get_next_number() {
+    /* register -> przechowywanie zmiennej w rejestrze procesora
+    znacznie szybsze niż RAM - brak adresu - nie można zrobić pointera  - nie można użyć w funkcjach */
+    register int input_char = gc();
+    while (input_char != EOF && input_char != ' ') {
+        if (input_char >= '0' && input_char <= '9')
+        {
+            current_number = (current_number << 1) + (current_number << 3) + (input_char - '0'); // == current_number * 10 + (c - '0')
+            read_chars_count += 1;
+        }
+        input_char = gc();
     }
-    else if (input_char == '\n')
-    {
-        //TODO: do testów lokalnych - do usunięcia później
-    }
-    else
-    { 
-        current_number = 0;
-    }
+    return current_number;
 }
