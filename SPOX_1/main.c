16b10f94d9d8b88ec74093bd65d8cbb6509ac5ec
Marcin Moch <Marcin Moch>
2017-03-31 22:15:40 +0200

    [SPOX_1]Reinvented algorithm. Much spaghetti but works!

--- SPOX_1/main.c
+++ SPOX_1/main.c
@@ -11,17 +11,16 @@ int read_chars = 0;
 int sum = 0;
 int max_length = 0;
 
-int max = -1;
-int min = -1;
-
 int current_sum = 0;
 int current_length = 0;
 
 int eq_len = 0;
 int eq_val = 0;
 
+int is_asc = 0;
+int is_desc = 0;
+int is_eq = 0;
 int is_start = 1;
-
 
 
 inline int get_next_number();
@@ -41,8 +40,8 @@ int main()
         }
     }
 
-    printf("Sum: %d read_chars: %d max_length: %d current_length: %d\n", sum, read_chars, max_length, current_length);
-    // printf("%d %d\n%d", max_length, sum, read_chars);
+    // printf("Sum: %d read_chars: %d max_length: %d current_length: %d\n", sum, read_chars, max_length, current_length);
+    printf("%d %d\n%d", max_length, sum, read_chars);
     return 0;
 }
 
@@ -66,92 +65,129 @@ inline int get_next_number()
 
 inline int algorithm(int current_number)
 {
-    if (min = -1)
-    {
-        min = current_number;
-    }
-    if (max = -1)
-    {
-        max = current_number;
-    }
-
     if (is_start)
     {
-        current_length = 1;
-        current_sum = current_number;
+        current_length += 1;
+        current_sum += current_number;
         is_start = 0;
     }
     else
     {
-        if (current_number == 7)
+        if (current_number < previous_number)
         {
-                printf("\n--->current_number: %d, min: %d, max: %d\n", current_number, min, max);
-        }
-        if (current_number > previous_number)
-        {
-            if (current_number > max)
+            if (is_desc)
             {
-                //rosnący
-                max = current_number;
+                current_length += 1;
+                current_sum += current_number;
+            }
+            else if (is_asc)
+            {
+                //zmiana trendu z + na -
+                if (current_length > max_length)
+                {
+                    //aktualny ciąg jest najdłuższy
+                    max_length = current_length;
+                    sum = current_sum;
+                }
+                //nowy ciąg
+                current_length = 0;
+                current_sum = 0;
+                if (is_eq && eq_len)
+                {
+                    current_length = eq_len;
+                    current_sum = (eq_len * eq_val);
+                    eq_len = 0;
+                    eq_val = 0;
+                }
+                if (!is_eq)
+                {
+                    //dodać poprzednią
+                    current_length = 1;
+                    current_sum = previous_number;
+                    //zerowanie eq
+                    eq_len = 0;
+                    eq_val = 0;
+                }
                 current_length += 1;
                 current_sum += current_number;
             }
             else
             {
-                //zmiana trendu (na rosnący z malejącego)
-                max = current_number;
-                min = -1;
-                current_length = 1;
-                if (eq_len)
-                {
-                    current_length += eq_len;
-                    current_sum += (eq_len * eq_val);
-                    eq_len = 0;
-                }
-
+                //prev is_eq
+                current_length += 1;
+                current_sum += current_number;
             }
+            //określenie trendu
+            is_desc = 1;
+            is_asc = 0;
+            is_eq = 0;
         }
-        else if (current_number < previous_number)
+        else if (current_number > previous_number)
         {
-            if (current_number < min)
+            if (is_asc)
             {
-                //malejący
-                min = current_number;
+                current_length += 1;
+                current_sum += current_number;
+            }
+            else if (is_desc)
+            {
+                //zmiana trendu z - na +
+                if (current_length > max_length)
+                {
+                    //aktualny ciąg jest najdłuższy
+                    max_length = current_length;
+                    sum = current_sum;
+                }
+                //nowy ciąg
+                current_length = 0;
+                current_sum = 0;
+                if (is_eq && eq_len)
+                {
+                    current_length = eq_len;
+                    current_sum = (eq_len * eq_val);
+                    eq_len = 0;
+                    eq_val = 0;
+                }
+                if (!is_eq)
+                {
+                    //dodać poprzednią
+                    current_length = 1;
+                    current_sum = previous_number;
+                    //zerowanie eq
+                    eq_len = 0;
+                    eq_val = 0;
+                }
                 current_length += 1;
                 current_sum += current_number;
             }
             else
             {
-                //zmiana trendu (na malejący z rosnącego)
-                max = -1;
-                min = current_number;
-                current_length = 1;
-                if (eq_len)
-                {
-                    current_length += eq_len;
-                    current_sum += (eq_len * eq_val);
-                    eq_len = 0;
-                }
-
+                //prev is_eq
+                current_length += 1;
+                current_sum += current_number;
             }
+            //określenie trendu
+            is_desc = 0;
+            is_asc = 1;
+            is_eq = 0;
         }
         else
         {
-            // current_number == previous_number
-            current_length += 1;
-            current_sum += current_number;
-            //eq
             if (eq_len == 0)
             {
                 eq_len = 1;
                 eq_val = current_number;
             }
             eq_len += 1;
-            
+            //trend boczny
+            current_length += 1;
+            current_sum += current_number;
+            //current_number == previous_number
+            is_eq = 1;
+
         }
     }
 
-    printf("\ncurrent_number: %d, current_sum: %d, current_length: %d, min: %d, max: %d\n", current_number, current_sum, current_length, min, max);
+    // printf("\ncurrent_number: %d, current_sum: %d, current_length: %d, is_asc: %d, is_desc: %d, is_eq: %d\n", current_number, current_sum, current_length, is_asc, is_desc, is_eq);
     previous_number = current_number;
-
 }
