180909e26d56e8a9be66a2931bc298e37671c198
Marcin Moch <Marcin.Moch@pkobp.pl>
2017-06-02 13:31:54 +0200

    [SPOX_3]Implemented Graph

--- SPOX_3/main.java
+++ SPOX_3/main.java
@@ -21,31 +21,71 @@ public class Main {
 //            e.printStackTrace();
 //        }
 //        System.out.print("ANSWER");
-        PriorityQueue pq = new PriorityQueue<>();
-        pq.add("TEST");
-        pq.add("DRUGI");
-        pq.add("TRZECI");
-        System.out.println(
-                pq.toString()
-        );
+
+        int verticesNumber = 3; //from stdinput
+        Graph graph = new Graph(verticesNumber);
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(0, 2);
+
+        System.out.println(graph.toString());
+
     }
 }
 
 class Graph {
-    public LinkedList<Vertex> vertices; //id == nr na liście stąd trzeba je dodawać w kolejności
+    public LinkedList<Vertex> vertices; //in order - id == position
 
-    public Graph() {
-        this.vertices.add(new Vertex(1));
-        this.vertices.get(1);
+    public Graph(int verticesNumber) {
+        this.vertices = new LinkedList<>();
+        for (int vertexId = 0; vertexId < verticesNumber; vertexId++) {
+            addVertex(vertexId);
+        }
+    }
+
+    public void addVertex(int vertexId) {
+        Vertex vertex = new Vertex(vertexId);
+        this.vertices.add(vertexId, vertex);
+    }
+
+    public void addEdge(int vertexId1, int vertexId2) {
+        Vertex vertex1 = this.vertices.get(vertexId1);
+        Vertex vertex2 = this.vertices.get(vertexId2);
+        vertex1.addEdge(vertex2);
+        vertex2.addEdge(vertex1);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Vertex vertex : this.vertices) {
+            sb.append(vertex.toString() + " | ");
+        }
+        return sb.toString();
     }
 
 }
 
 class Vertex {
     private int id;
-    private HashSet<Vertex> edges; //particular Vertex edges
+    private HashSet<Vertex> edges; //not in order
 
     public Vertex(int id) {
+        this.edges = new HashSet<>();
         this.id = id;
+    }
+
+    public void addEdge(Vertex vertex) {
+        this.edges.add(vertex);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[" + this.id + "] -> ");
+        for (Vertex vertex : this.edges) {
+            sb.append(vertex.id + ", ");
+        }
+        return sb.toString();
     }
 }
